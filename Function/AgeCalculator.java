93be8eb5acb5e07be8b8e48d5ca83246cabaa1b5
Shivam... <shivam243028@gmail.com>
2026-08-11 21:47:04 +0530

    AgeCalculator

--- Function/AgeCalculator.java
+++ Function/AgeCalculator.java
@@ -5,7 +5,6 @@ public class AgeCalculator {
     static int calculateAge(int birthYear, int currentYear) {
         return currentYear - birthYear;
     }
-
     public static void main(String[] args) {
 
         try (Scanner sc = new Scanner(System.in)) {
