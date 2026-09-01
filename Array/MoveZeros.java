cc08bc45ad1794a9a15950dc760a45becb6525ac
Shivam... <shivam243028@gmail.com>
2026-09-01 22:41:51 +0530

    MoveZeros

---
+++ Array/MoveZeros.java
@@ -0,0 +1,24 @@
+public class MoveZeros {
+     public static void main(String[] args) {
+
+        int[] arr = {0, 1, 0, 3, 12};
+
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] != 0) {
+
+                int temp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = temp;
+
+                index++;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
