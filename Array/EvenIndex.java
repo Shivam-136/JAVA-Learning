16c02497a40efebc54773ecb999da91657adecb7
Shivam Chhanchhan <shivam243028@gmail.com>
2026-09-04 23:58:19 +0530

    EvenIndex

--- Array/EvenIndex.java
+++ Array/EvenIndex.java
@@ -2,7 +2,6 @@ public class EvenIndex {
     public static void main(String[] args) {
 
         int[] arr = {10, 20, 30, 40, 50, 60};
-
         System.out.println("Elements at Even Index:");
 
         for (int i = 0; i < arr.length; i += 2) {
