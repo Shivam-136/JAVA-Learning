9041cf511a95fbe1d4ccccf3bf039b856133073c
Shivam Chhanchhan <shivam243028@gmail.com>
2026-09-04 23:56:48 +0530

    LargestElement

--- Array/LargestElement.java
+++ Array/LargestElement.java
@@ -1,8 +1,8 @@
 public class LargestElement {
     public static void main(String[] args) {
-
+        
         int[] arr = {10, 50, 30, 80, 20};
-
+        
         int max = arr[0];
 
         for (int i = 1; i < arr.length; i++) {
